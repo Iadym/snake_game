e81f75c8654fd69756094dd8599a2114768963cb
Masha Belova <mbelova@salesforce.com>
2021-03-25 15:02:43 -0400

    third lesson

---
+++ main.py
@@ -0,0 +1,5 @@
+from snake import Game
+
+if __name__ == '__main__':
+    snake = Game()
+    snake.run()
--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 import random
+import time
 
 WINDOW_HEIGHT = 480
 WINDOW_WIDTH = 640
@@ -20,16 +21,16 @@ class Window():
 
     def draw_snake(self, game, snake_body):
         for pixel in snake_body:
-            game.draw.rect(self, GREEN_COLOR, game.Rect(pixel[0], pixel[1], 10, 10))
+            game.draw.rect(self.window, GREEN_COLOR, game.Rect(pixel[0], pixel[1], 10, 10))
 
     def draw_food(self, game, food):
-        game.draw.rect(self, BLUE_COLOR, game.Rect(food[0], food[1], 8, 8))
+        game.draw.rect(self.window, BLUE_COLOR, game.Rect(food[0], food[1], 10, 10))
 
     def draw_score(self, game, score):
         SCORE_FONT = game.font.SysFont('Times New Roman', 20)
         score_surface = SCORE_FONT.render(f'Score: ${score}', True, WHITE_COLOR)
         score_rect = score_surface.get_rect()
-        score_rect.midtop = (WINDOW_WIDTH // 2, 20)
+        score_rect.midtop = (WINDOW_WIDTH // 2, 15)
         self.window.blit(score_surface, score_rect)
 
     def draw_game_over(self, game, exit_game):
@@ -39,6 +40,8 @@ class Window():
         final_rect.midtop = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
         self.window.fill(BLACK_COLOR)
         self.window.blit(final_surface, final_rect)
+        game.display.update()
+        time.sleep(10)
         exit_game()
 
 class Snake:
@@ -51,7 +54,7 @@ class Snake:
             [self.snake_head[0] - 20, self.snake_head[1]]
         ]
 
-    def snake_change_direction(self, new_direction: str):
+    def snake_change_direction(self, new_direction):
         if new_direction == self.snake_direction:
             return
         if self.snake_direction == 'DOWN' and new_direction == 'UP':
@@ -66,14 +69,14 @@ class Snake:
 
     def snake_move(self):
         if self.snake_direction == 'UP':
-            new_snake_head = [self.snake_head[0], self.snake_head[1] + 10]
+            self.snake_head = [self.snake_head[0], self.snake_head[1] - 10]
         if self.snake_direction == 'DOWN':
-            new_snake_head = [self.snake_head[0], self.snake_head[1] - 10]
+            self.snake_head = [self.snake_head[0], self.snake_head[1] + 10]
         if self.snake_direction == 'RIGHT':
-            new_snake_head = [self.snake_head[0] + 10, self.snake_head[1]]
+            self.snake_head = [self.snake_head[0] + 10, self.snake_head[1]]
         if self.snake_direction == 'LEFT':
-            new_snake_head = [self.snake_head[0] - 10, self.snake_head[1]]
-        self.snake_body.insert(0, new_snake_head)
+            self.snake_head = [self.snake_head[0] - 10, self.snake_head[1]]
+        self.snake_body.insert(0, self.snake_head)
         self.snake_body.pop()
 
     def snake_grow(self):
@@ -97,26 +100,29 @@ class Score:
 
 class Game:
     def __init__(self):
-        # initializing all the Game objects
-        self.snake = Snake()
-        self.food = Food()
-        self.score = Score()
         # initializing game vars
         self.game = pygame
         self.game.init()
         self.game.display.set_caption('Snake')
+        # initializing all the Game objects
+        self.fps = pygame.time.Clock()
+        self.snake = Snake()
+        self.food = Food()
+        self.score = Score()
         # making the game window appeared on the screen
         self.window = Window(self.game.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT)))
 
-    def run(self):
-        self.make_snake_turn()
-        self.window.draw_stage()
-        self.window.draw_snake()
-        self.window.draw_food()
-        self.window.draw_score()
-        self.window.draw_game_over()
-        self.game.display.update()
-        self.food_control()
+    def game_over(self):
+        # snake head running into the borders of the window
+        if self.snake.snake_head[0] < 0 or self.snake.snake_head[0] > WINDOW_WIDTH - 10:
+            self.window.draw_game_over(self.game, self.exit_game)
+        if self.snake.snake_head[1] < 0 or self.snake.snake_head[1] > WINDOW_HEIGHT - 10:
+            self.window.draw_game_over(self.game, self.exit_game)
+
+        # snake is running into itself
+        for block in self.snake.snake_body[1:]:
+            if block[0] == self.snake.snake_head[0] and block[1] == self.snake.snake_head[1]:
+                self.window.draw_game_over(self.game, self.exit_game)
 
     def exit_game(self):
         self.game.quit()
@@ -130,28 +136,29 @@ class Game:
             self.food.food_respawn()
             self.score.increase()
 
-    def make_snake_turn(self):
+    def run(self):
         while True:
             for event in self.game.event.get():
-                if event.type == self.game.QUIT or event.type == self.game.K_ESCAPE:
+                if event.type == self.game.QUIT:
                     self.exit_game()
-                elif event.type == self.game.K_DOWN or event.type == self.game.K_s:
-                    self.snake.snake_change_direction('DOWN')
-                elif event.type == self.game.K_UP or event.type == self.game.K_n:
-                    self.snake.snake_change_direction('UP')
-                elif event.type == self.game.K_RIGHT or event.type == self.game.K_e:
-                    self.snake.snake_change_direction('RIGHT')
-                elif event.type == self.game.K_LEFT or event.type == self.game.K_w:
-                    self.snake.snake_change_direction('LEFT')
+                elif event.type == self.game.KEYDOWN:
+                    if event.key == self.game.K_ESCAPE:
+                        self.exit_game()
+                    else:
+                        if event.key == self.game.K_DOWN or event.key == self.game.K_s:
+                            self.snake.snake_change_direction('DOWN')
+                        if event.key == self.game.K_UP or event.key == self.game.K_w:
+                            self.snake.snake_change_direction('UP')
+                        if event.key == self.game.K_LEFT or event.key == self.game.K_a:
+                            self.snake.snake_change_direction('LEFT')
+                        if event.key == self.game.K_RIGHT or event.key == self.game.K_d:
+                            self.snake.snake_change_direction('RIGHT')
 
-    def game_over(self):
-        #snake head running into the borders of the window
-        if self.snake.snake_head[0] < 0 or self.snake.snake_head[0] > WINDOW_WIDTH:
-            self.window.draw_game_over(self.game, self.exit_game)
-        if self.snake.snake_head[1] < 0 or self.snake.snake_head[1] > WINDOW_HEIGHT:
-            self.window.draw_game_over(self.game, self.exit_game)
-
-        #snake is running into itself
-        for block in self.snake.snake_body[1:]:
-            if block[0] == self.snake.snake_head[0] and block[1] == self.snake.snake_head[1]:
-                self.window.draw_game_over(self.game, self.exit_game)
+            self.food_control()
+            self.game_over()
+            self.window.draw_stage()
+            self.window.draw_snake(self.game, self.snake.snake_body)
+            self.window.draw_food(self.game, self.food.position)
+            self.window.draw_score(self.game, self.score.score)
+            self.game.display.update()
+            self.fps.tick(20)
